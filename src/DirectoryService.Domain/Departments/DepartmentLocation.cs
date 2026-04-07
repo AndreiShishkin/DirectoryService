f609f2a4bbe66cff06689f7577808237e63d6d1c
Shishkin Andrey <Andreishishkin99@mail.ru>
2026-04-07 21:03:35 +0400

    Добавил дополнительные сущности для реализации связи многие ко многим между сущностями Department и Location, Department и Position

---
+++ src/DirectoryService.Domain/Departments/DepartmentLocation.cs
@@ -0,0 +1,17 @@
+﻿namespace DirectoryService.Domain.Departments;
+
+public record DepartmentLocation
+{
+    public Guid Id { get; }
+
+    public Department Department { get; private set; }
+
+    public Guid LocationId { get; private set; }
+
+    public DepartmentLocation(Guid? id, Department department, Guid locationId)
+    {
+        Id = id ?? Guid.NewGuid();
+        Department = department;
+        LocationId = locationId;
+    }
+}
---
+++ src/DirectoryService.Domain/Departments/DepartmentPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace DirectoryService.Domain.Departments;
+
+public record DepartmentPosition
+{
+    public Guid Id { get; }
+
+    public Department Department { get; private set; }
+
+    public Guid PositionId { get; private set; }
+
+    public DepartmentPosition(Guid? id, Department department, Guid positionId)
+    {
+        Id = id ?? Guid.NewGuid();
+        Department = department;
+        PositionId = positionId;
+    }
+}
