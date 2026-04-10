d8b4be904bb8a94b6bb86e2d963121375ae4516a
Shishkin Andrey <Andreishishkin99@mail.ru>
2026-04-10 20:13:38 +0400

    Добавил связи DepartmentLocation и DepartmentPosition в классе Department

--- src/DirectoryService.Domain/Departments/Department.cs
+++ src/DirectoryService.Domain/Departments/Department.cs
@@ -7,13 +7,20 @@ namespace DirectoryService.Domain.Departments;
 
 public class Department
 {
+    private List<DepartmentLocation> _locations;
+    private List<DepartmentPosition> _positions;
+
     public Guid Id { get; }
 
-    public DepartmentName DepartmentName { get; private set;  }
+    public DepartmentName DepartmentName { get; private set; }
 
     public Identifier Identifier { get; private set; }
 
     public Guid? ParentId { get; private set; }
+
+    public IReadOnlyList<DepartmentLocation> Locations => _locations;
+
+    public IReadOnlyList<DepartmentPosition> Positions => _positions;
 
     public Path Path { get; private set; }
 
@@ -30,6 +37,8 @@ public class Department
         DepartmentName departmentName,
         Identifier identifier,
         Guid? parentId,
+        IEnumerable<Guid> locationIds,
+        IEnumerable<Guid> positionIds,
         Path path,
         short depth)
     {
@@ -42,15 +51,27 @@ public class Department
         IsActive = true;
         CreatedAt = DateTime.UtcNow;
         UpdatedAt = CreatedAt;
+
+        var departmentLocations = locationIds.Select(locationId =>
+                new DepartmentLocation(Guid.NewGuid(), this, locationId))
+            .ToList();
+        _locations = departmentLocations;
+
+        var departmentPositions = positionIds.Select(positionId =>
+                new DepartmentPosition(Guid.NewGuid(), this, positionId))
+            .ToList();
+        _positions = departmentPositions;
     }
 
     public static Result<Department, Error> Create(
         DepartmentName name,
         Identifier identifier,
         Guid? parentId,
+        IEnumerable<Guid> locationIds,
+        IEnumerable<Guid> positionIds,
         Path path,
         short depth)
     {
-        return new Department(Guid.NewGuid(), name, identifier, parentId, path, depth);
+        return new Department(Guid.NewGuid(), name, identifier, parentId, locationIds, positionIds, path, depth);
     }
 }
